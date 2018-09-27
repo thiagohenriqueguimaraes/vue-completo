79befdba1f53cee7537188ae7ebd22d6d7bfbe1b
Thiago <thiago@tribecca.com.br>
2018-09-27 17:04:51 -0300

    layout boottrap

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <template>
   <div id="app">
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault"></div>
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+                  <router-link class="nav-link" to="/">Home</router-link> |
+            <!-- <a class="nav-link" href="\">Home <span class="sr-only">(current)</span></a> -->
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="\about">About</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/produto">Produtos</router-link>  
+      <router-link class="nav-link" to="/">Home</router-link> |
+      <router-link to="/about">Sobre</router-link> |
+      <router-link to="/produto">Produtos</router-link>
     </div>
     <router-view/>
   </div>
--- src/views/Produto.vue
+++ src/views/Produto.vue
@@ -1,16 +1,66 @@
 <template>
   <div class="produto">
+    <h1>Lista de produtos</h1>
+    <div class="row">
+      <div class="col-md-12">
+        <table style="width:100%">
+              <tr>
+                <th>Códito</th>
+                <th>Nome</th> 
+              </tr>
+              <tbody>
+                <gridProduto
+                  v-for="p in [{ id: 1, name: 'A' },{ id: 2, name: 'B' }, { id: 3, name: 'C' }]"
+                  v-bind:key="p.id"
+                  v-bind:p="p"
+                ></gridProduto>
+              </tbody> 
+
+            </table>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <table style="width:100%">
+              <tr>
+                <th>Código</th>
+                <th>Nome</th> 
+              </tr>
+              <tbody>
+                <tr v-for="x in xs">
+                  <td>{{x.y}}</td>
+                  <td></td>                  
+                </tr>
+              </tbody> 
+
+            </table>
+      </div>
+    </div>
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Produto from '@/components/Produto.vue'
+import gridProduto from '@/components/gridProduto.vue'
 
 export default {
   name: 'produto',
+  data () {
+    return {
+      xs : [{y:1}]
+    }
+  },
   components: {
-    Produto
+    gridProduto
   }
 }
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+}
+</style>
